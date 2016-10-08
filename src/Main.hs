a12d2bfdd3a104bc662ad6e5b6c1ad537915f17c
David Grayson <davidegrayson@gmail.com>
2016-10-07 19:32:36 -0700

    Got it to read a file and print it.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Parser ()
+import Data.Text.IO (readFile, putStr)
+import Prelude hiding (readFile, putStr)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  [drvFilenameA, drvFilenameB] <- getArgs
+  t <- readFile drvFilenameA
+  putStr t
