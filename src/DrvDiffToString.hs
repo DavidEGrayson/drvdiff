30b62491896e16c1d9c93a277eb3bdd4ac72a13f
David Grayson <davidegrayson@gmail.com>
2016-12-17 17:14:31 -0800

    DrvDiffToString: Starting to look nice.

--- src/DrvDiffToString.hs
+++ src/DrvDiffToString.hs
@@ -1,12 +1,21 @@
 module DrvDiffToString (drvDiffToString) where
 
 import DrvDiff
+import Derivation
+import Data.List
 
 drvDiffToString :: DrvDiff -> String
 drvDiffToString (DrvDiff l r) =
-  (drvPartToString "- " l) ++ "\n" ++
-  (drvPartToString "+ " r) ++ "\n"
+  (drvPartToString "- " l) ++
+  (drvPartToString "+ " r)
 
 drvPartToString :: String -> DrvPart -> String
-drvPartToString prefix part =
-  prefix ++ (show (drvPartOutputs part))
+drvPartToString prefix part = let
+    lines = (map drvOutputToString (drvPartOutputs part))
+    lineFixer = \x -> prefix ++ x ++ "\n"
+  in
+    intercalate "" (map lineFixer lines)
+
+drvOutputToString :: DerivationOutput -> String
+drvOutputToString (DerivationOutput name path _ _) =
+  "Output: " ++ name ++ ": " ++ path
