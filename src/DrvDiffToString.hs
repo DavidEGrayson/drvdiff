121d8fed59c12f0e4b38fa993b35ecef16067f33
David Grayson <davidegrayson@gmail.com>
2016-12-17 17:32:22 -0800

    The output is finally complete and useful.

--- src/DrvDiffToString.hs
+++ src/DrvDiffToString.hs
@@ -3,6 +3,7 @@ module DrvDiffToString (drvDiffToString) where
 import DrvDiff
 import Derivation
 import Data.List
+import Data.Maybe
 
 drvDiffToString :: DrvDiff -> String
 drvDiffToString (DrvDiff l r) =
@@ -10,12 +11,39 @@ drvDiffToString (DrvDiff l r) =
   (drvPartToString "+ " r)
 
 drvPartToString :: String -> DrvPart -> String
-drvPartToString prefix part = let
-    lines = (map drvOutputToString (drvPartOutputs part))
-    lineFixer = \x -> prefix ++ x ++ "\n"
-  in
-    intercalate "" (map lineFixer lines)
+drvPartToString prefix p =
+  joinLines prefix (
+    (fmap drvOutputToString (drvPartOutputs p)) ++
+    (fmap drvInputToString (drvPartInputs p)) ++
+    (fmap drvSourceToString (drvPartSources p)) ++
+    maybeToList (fmap drvSystemToString (drvPartSystem p)) ++
+    maybeToList (fmap drvBuilderToString (drvPartBuilder p)) ++
+    maybeToList (fmap drvArgsToString (drvPartArgs p)) ++
+    (fmap drvEnvVarToString (drvPartEnv p))
+  )
+
+joinLines :: String -> [String] -> String
+joinLines prefix ls = intercalate "" (map (\x -> prefix ++ x ++ "\n") ls)
 
 drvOutputToString :: DerivationOutput -> String
 drvOutputToString (DerivationOutput name path _ _) =
   "Output: " ++ name ++ ": " ++ path
+
+drvInputToString :: DerivationInput -> String
+drvInputToString (DerivationInput path names) =
+  "Input: " ++ path ++ " " ++ (intercalate "," names)
+
+drvSourceToString :: String -> String
+drvSourceToString s = "source: " ++ s
+
+drvSystemToString :: String -> String
+drvSystemToString s = "system: " ++ s
+
+drvBuilderToString :: String -> String
+drvBuilderToString b = "builder: " ++ b
+
+drvArgsToString :: [String] -> String
+drvArgsToString args = "args: " ++ intercalate " " args
+
+drvEnvVarToString :: DerivationEnvVar -> String
+drvEnvVarToString (DerivationEnvVar name value) = "env: " ++ name ++ " = " ++ (show value)
