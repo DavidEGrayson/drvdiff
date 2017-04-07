76aed32e27f028b377152aa16bf829601c819305
David Grayson <davidegrayson@gmail.com>
2017-04-06 19:29:34 -0700

    Simplified makeDrvDiff with help from LVFPUG.

--- src/DrvDiffMake.hs
+++ src/DrvDiffMake.hs
@@ -8,29 +8,21 @@ makeDrvDiff :: Derivation -> Derivation -> DrvDiff
 makeDrvDiff l r =
   DrvDiff
     (DrvPart
-      outputsLeft
-      inputsLeft
-      sourcesLeft
-      systemLeft
-      builderLeft
-      argsLeft
-      envLeft)
+      outputsLeft inputsLeft sourcesLeft
+      systemLeft builderLeft argsLeft envLeft)
     (DrvPart
-      outputsRight
-      inputsRight
-      sourcesRight
-      systemRight
-      builderRight
-      argsRight
-      envRight)
+      outputsRight inputsRight sourcesRight
+      systemRight builderRight argsRight envRight)
   where
-    (outputsLeft, outputsRight) = listDiff (drvOutputs l) (drvOutputs r)
-    (inputsLeft, inputsRight) = listDiff (drvInputs l) (drvInputs r)
-    (sourcesLeft, sourcesRight) = listDiff (drvSources l) (drvSources r)
-    (systemLeft, systemRight) = itemDiff (drvSystem l) (drvSystem r)
-    (builderLeft, builderRight) = itemDiff (drvBuilder l) (drvBuilder r)
-    (argsLeft, argsRight) = itemDiff (drvArgs l) (drvArgs r)
-    (envLeft, envRight) = listDiff (drvEnv l) (drvEnv r)
+    (outputsLeft, outputsRight) = diffWith listDiff drvOutputs
+    (inputsLeft, inputsRight) = diffWith listDiff drvInputs
+    (sourcesLeft, sourcesRight) = diffWith listDiff drvSources
+    (systemLeft, systemRight) = diffWith itemDiff drvSystem
+    (builderLeft, builderRight) = diffWith itemDiff drvBuilder
+    (argsLeft, argsRight) = diffWith itemDiff drvArgs
+    (envLeft, envRight) = diffWith listDiff drvEnv
+    diffWith :: (p -> p -> d) -> (Derivation -> p) -> d
+    diffWith d f = d (f l) (f r)
 
 listDiff :: Eq t => [t] -> [t] -> ([t], [t])
 listDiff l r = (l \\ r, r \\ l)
